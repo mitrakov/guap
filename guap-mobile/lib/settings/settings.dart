b004e1e69450b2a1b3d2aa528d221f628c842ffc
Tommy-Air <tommy@air.ru>
2025-12-31 14:49:02 +0300

    Make RUB main currency. No build, no release

--- guap-mobile/lib/settings/settings.dart
+++ guap-mobile/lib/settings/settings.dart
@@ -10,38 +10,39 @@ class Settings {
   }
 
   static bool showPersons() {
-    _check();
+    _check(); // TODO remove check, use singleton pattern
     return (_storage!.containsKey(_showPersonsKey) ? _storage!.getBool(_showPersonsKey) : true) ?? true; // TODO update for null-safety
   }
 
   static void setShowPersons(bool value) {
-    _check();
+    _check(); // TODO remove check, use singleton pattern
     _storage!.setBool(_showPersonsKey, value);
   }
 
   static double getCurrencyRate(String currency) {
-    _check();
+    _check(); // TODO remove check, use singleton pattern
     if (_storage!.containsKey("$_currencyRatesKey$currency"))
       return _storage!.getDouble("$_currencyRatesKey$currency") ?? 1;
-    switch (currency) {          // data by Forbes 2024-03-10
-      case "RUB": return 0.010995;
-      case "USD": return 1;
-      case "EUR": return 1.09435;
-      case "GBP": return 1.2858;
-      case "KGS": return 0.01141;
-      case "AMD": return 0.002481;
-      case "THB": return 0.028287;
-      case "INR": return 0.012084;
+    switch (currency) {          // data by cbr.ru 2026-01-01
+      case "RUB": return 1;
+      case "USD": return 78.2267;
+      case "EUR": return 92.0938;
+      case "GBP": return 105.6060;
+      case "KGS": return 0.894861;
+      case "AMD": return 0.205126;
+      case "THB": return 2.48095;
+      case "INR": return 0.869737;
       default:    return 1;      // default no-op multiplicator
     }
   }
 
   static void setCurrencyRate(String currency, double value) {
-    _check();
+    _check(); // TODO remove check, use singleton pattern
     _storage!.setDouble("$_currencyRatesKey$currency", value);
   }
 
   static void _check() {
+    // TODO remove check, use singleton pattern
     if (_storage == null)
       throw Exception("Call Settings.init() in main app widget");
   }
--- guap-mobile/lib/settings/settingswidget.dart
+++ guap-mobile/lib/settings/settingswidget.dart
@@ -7,13 +7,14 @@ class SettingsWidget extends StatefulWidget { // StatefulWidget needed for setSt
 }
 
 class SettingsWidgetState extends State<SettingsWidget> {
-  final rubToUsdCtrl = TextEditingController(text: Settings.getCurrencyRate("RUB").toString());
-  final eurToUsdCtrl = TextEditingController(text: Settings.getCurrencyRate("EUR").toString());
-  final gbpToUsdCtrl = TextEditingController(text: Settings.getCurrencyRate("GBP").toString());
-  final kgsToUsdCtrl = TextEditingController(text: Settings.getCurrencyRate("KGS").toString());
-  final amdToUsdCtrl = TextEditingController(text: Settings.getCurrencyRate("AMD").toString());
-  final thbToUsdCtrl = TextEditingController(text: Settings.getCurrencyRate("THB").toString());
-  final inrToUsdCtrl = TextEditingController(text: Settings.getCurrencyRate("INR").toString());
+  final rubCtrl = TextEditingController(text: Settings.getCurrencyRate("RUB").toString());
+  final usdCtrl = TextEditingController(text: Settings.getCurrencyRate("USD").toString());
+  final eurCtrl = TextEditingController(text: Settings.getCurrencyRate("EUR").toString());
+  final gbpCtrl = TextEditingController(text: Settings.getCurrencyRate("GBP").toString());
+  final kgsCtrl = TextEditingController(text: Settings.getCurrencyRate("KGS").toString());
+  final amdCtrl = TextEditingController(text: Settings.getCurrencyRate("AMD").toString());
+  final thbCtrl = TextEditingController(text: Settings.getCurrencyRate("THB").toString());
+  final inrCtrl = TextEditingController(text: Settings.getCurrencyRate("INR").toString());
 
   @override
   Widget build(BuildContext context) {
@@ -31,38 +32,43 @@ class SettingsWidgetState extends State<SettingsWidget> {
         ),
         ListTile(
           title: const Text("RUB rate"),
-          subtitle: const Text("Currency rate RUB to USD"),
-          trailing: SizedBox(width: 100, child: TextField(controller: rubToUsdCtrl, onChanged: (s) => _setCurrency("RUB", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
+          subtitle: const Text("Currency rate RUB"),
+          trailing: SizedBox(width: 110, child: TextField(controller: rubCtrl, onChanged: (s) => _setCurrency("RUB", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
+        ),
+        ListTile(
+          title: const Text("USD rate"),
+          subtitle: const Text("Currency rate USD"),
+          trailing: SizedBox(width: 110, child: TextField(controller: usdCtrl, onChanged: (s) => _setCurrency("USD", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
         ),
         ListTile(
           title: const Text("EUR rate"),
-          subtitle: const Text("Currency rate EUR to USD"),
-          trailing: SizedBox(width: 100, child: TextField(controller: eurToUsdCtrl, onChanged: (s) => _setCurrency("EUR", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
+          subtitle: const Text("Currency rate EUR"),
+          trailing: SizedBox(width: 110, child: TextField(controller: eurCtrl, onChanged: (s) => _setCurrency("EUR", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
         ),
         ListTile(
           title: const Text("GBP rate"),
-          subtitle: const Text("Currency rate GBP to USD"),
-          trailing: SizedBox(width: 100, child: TextField(controller: gbpToUsdCtrl, onChanged: (s) => _setCurrency("GBP", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
+          subtitle: const Text("Currency rate GBP"),
+          trailing: SizedBox(width: 110, child: TextField(controller: gbpCtrl, onChanged: (s) => _setCurrency("GBP", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
         ),
         ListTile(
           title: const Text("KGS rate"),
-          subtitle: const Text("Currency rate KGS to USD"),
-          trailing: SizedBox(width: 100, child: TextField(controller: kgsToUsdCtrl, onChanged: (s) => _setCurrency("KGS", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
+          subtitle: const Text("Currency rate KGS"),
+          trailing: SizedBox(width: 110, child: TextField(controller: kgsCtrl, onChanged: (s) => _setCurrency("KGS", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
         ),
         ListTile(
           title: const Text("AMD rate"),
-          subtitle: const Text("Currency rate AMD to USD"),
-          trailing: SizedBox(width: 100, child: TextField(controller: amdToUsdCtrl, onChanged: (s) => _setCurrency("AMD", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
+          subtitle: const Text("Currency rate AMD"),
+          trailing: SizedBox(width: 110, child: TextField(controller: amdCtrl, onChanged: (s) => _setCurrency("AMD", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
         ),
         ListTile(
           title: const Text("THB rate"),
-          subtitle: const Text("Currency rate THB to USD"),
-          trailing: SizedBox(width: 100, child: TextField(controller: thbToUsdCtrl, onChanged: (s) => _setCurrency("THB", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
+          subtitle: const Text("Currency rate THB"),
+          trailing: SizedBox(width: 110, child: TextField(controller: thbCtrl, onChanged: (s) => _setCurrency("THB", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
         ),
         ListTile(
           title: const Text("INR rate"),
-          subtitle: const Text("Currency rate INR to USD"),
-          trailing: SizedBox(width: 100, child: TextField(controller: inrToUsdCtrl, onChanged: (s) => _setCurrency("INR", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
+          subtitle: const Text("Currency rate INR"),
+          trailing: SizedBox(width: 110, child: TextField(controller: inrCtrl, onChanged: (s) => _setCurrency("INR", s), decoration: const InputDecoration(border: OutlineInputBorder()))),
         ),
       ]
     );
